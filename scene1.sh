357bf38b04f4a5929f550f69efa30d1dd5a75901
Swathi Mamidi <swathimamidi29@gmail.com>
2018-02-20 15:58:36 +0530

    installing docker

---
+++ scene1.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+yum remove docker docker-common docker-selinex docker-engine
+yum install -y yum-utils device-mapper-persistent-data lvm2
+yum-config-manager --add repo https://download.docker.com/linux/centos/docker-ce.repo
+yum install docker-ce -y -y
+systemctl start docker

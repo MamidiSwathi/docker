4b3fa9f610a02cd518c1838810617170a53571ce
MamidiSwathi <36503251+MamidiSwathi@users.noreply.github.com>
2018-02-20 10:57:36 +0000

    Update scene1.sh

--- scene1.sh
+++ scene1.sh
@@ -1,11 +1,6 @@
 #!/bin/sh
-yum remove docker docker-common docker-selinex docker-engine docker-ce
-yum install -y yum-utils device-mapper-persistent-data lvm2
-yum-config-manager --add repo https://download.docker.com/linux/centos/docker-ce.repo
-yum install docker-ce -y -y
-systemctl start docker
+
 docker run -it centos
-yum install httpd -y 
-echo success 
+
 
 

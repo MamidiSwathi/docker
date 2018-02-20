fa7fc3b823ebe7fa682c7b00e5783bfe6a8ddb67
MamidiSwathi <36503251+MamidiSwathi@users.noreply.github.com>
2018-02-20 11:14:21 +0000

    Update scene1.sh

--- scene1.sh
+++ scene1.sh
@@ -4,7 +4,7 @@ yum install -y yum-utils device-mapper-persistent-data lvm2
 yum-config-manager --add repo https://download.docker.com/linux/centos/docker-ce.repo
 yum install docker-ce -y -y
 systemctl start docker
-docker rexec -it 549fb85c960f /bin/bash
+docker exec -it 549fb85c960f /bin/bash
 yum install httpd -y 
 service httpd start
 service httpd status

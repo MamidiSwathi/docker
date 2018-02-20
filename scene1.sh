9d7ff3069e388e2929e2140039bdb6cea9f0a24f
MamidiSwathi <36503251+MamidiSwathi@users.noreply.github.com>
2018-02-20 10:38:13 +0000

    Update scene1.sh

--- scene1.sh
+++ scene1.sh
@@ -4,3 +4,4 @@ yum install -y yum-utils device-mapper-persistent-data lvm2
 yum-config-manager --add repo https://download.docker.com/linux/centos/docker-ce.repo
 yum install docker-ce -y -y
 systemctl start docker
+echo success
